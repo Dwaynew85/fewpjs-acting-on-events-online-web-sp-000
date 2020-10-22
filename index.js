8ae9d4fa370e2fc2a636e22d82d8ab8db100f2c9
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-10-22 20:42:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,8 +8,19 @@ function moveDodgerLeft() {
   }
 }
 
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
+  }else if (e.key === "ArrowRight") {
+    moveDodgerRight();
   }
 });
